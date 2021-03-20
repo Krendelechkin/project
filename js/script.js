2b197d9cafeff1a3ce5e519fa328d1861df6ec35
Dimitrij <korolev@tut.by>
2021-03-20 17:59:03 +0300

    Add js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
-const obj = {
- f: 1
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB= {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+
 };
-console.log(obj);
+
+const a=prompt('Один из последних просмотренных фильмов?', ''),
+      b=prompt('На сколько оцените его?', ''),
+      c=prompt('Один из последних просмотренных фильмов?', ''),
+      d=prompt('На сколько оцените его?', '');
+
+      personalMovieDB.movies[a]=b;
+      personalMovieDB.movies[c]=d;
+
+      console.log(personalMovieDB);
